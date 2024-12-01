d33043e6548e67ade7c6729f86a629c43964b3fc
DDlia <gh@ddlia.com>
2024-12-01 21:55:20 +0900

    force runtime = 'edge';

--- pages/api/search-notion.ts
+++ pages/api/search-notion.ts
@@ -3,6 +3,8 @@ import { type NextApiRequest, type NextApiResponse } from 'next'
 import type * as types from '../../lib/types'
 import { search } from '../../lib/notion'
 
+export const runtime = 'edge';
+
 export default async function searchNotion(
   req: NextApiRequest,
   res: NextApiResponse
